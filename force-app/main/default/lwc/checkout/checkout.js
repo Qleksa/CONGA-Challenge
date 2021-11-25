57381b537b492a7657e8f5d5837f1df8567f0520
Aleksandar Petrovic <qleksa@gmail.com>
2021-11-25 09:33:17 +0100

    checkout fix

--- force-app/main/default/lwc/checkout/checkout.js
+++ force-app/main/default/lwc/checkout/checkout.js
@@ -2,9 +2,24 @@ import { LightningElement } from 'lwc';
 
 import getProducts from '@salesforce/apex/ProductController.getProducts';
 
+const columns = [
+    { type:'customImage', fixedWidth: 150,
+        typeAttributes: {
+            imageUrl: { fieldName: 'Image_URL__c' }
+        } 
+    },
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Description', fieldName: 'Description' },
+    { label: 'Color', fieldName: 'Color__c' },
+    { label: 'Dimension', fieldName: 'Dimension__c' },
+    { label: 'Price', type: 'currency', fieldName: 'Price__c' } 
+];
+
 export default class Checkout extends LightningElement {
     products;
     totalPrice = 0;
+
+    columns = columns;
 
     connectedCallback() {
         let cartProductIds = localStorage.getItem('cartProductIds');
